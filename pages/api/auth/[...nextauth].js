2957ccf46d02a2f8516e2b230cee61f34779e460
devaryanmittal <aryanmittal2017@gmail.com>
2023-10-08 14:25:39 +0530

    app built

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,40 @@
+import clientPromise from '@/lib/mongodb'
+import { MongoDBAdapter } from '@auth/mongodb-adapter'
+import NextAuth, {getServerSession} from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+
+const adminEmails = ['aryanmittal2017@gmail.com'];
+
+export const authOptions = {
+  
+  secret: process.env.SECRET,
+
+  providers: [
+    // OAuth authentication providers...
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    }),
+  ],
+  adapter: MongoDBAdapter(clientPromise),
+  callbacks: {
+    session: ({session,token,user}) => {
+      if (adminEmails.includes(session?.user?.email)) {
+        return session;
+      } else {
+        return false;
+      }
+    },
+  },
+};
+
+export default NextAuth(authOptions);
+
+export async function isAdminRequest(req,res) {
+  const session = await getServerSession(req,res,authOptions);
+  if (!adminEmails.includes(session?.user?.email)) {
+    res.status(401);
+    res.end();
+    throw 'not an admin';
+  }
+}
